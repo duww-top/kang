bda3e1fa6d7e587fe2b4e461ec13a5f2b01f936f
永恒前夕 <Master@evec.cc>
2018-03-20 14:33:03 +0000

    Update Master.sh

--- Master.sh
+++ Master.sh
@@ -10,9 +10,6 @@ mkdir $L;
 chmod 755 $L;
 fi
 
-mkdir /usr/evekangle/
-touch /usr/evekangle/lock.img
-
 if [ ! -d "/usr/evekangle/lock.img" ];
 then
 echo -e "检测到之前已经执行过本脚本，出于安全考虑，已经停止脚本，如需执行请删除/usr/evekangle/lock.img文件"
@@ -28,3 +25,6 @@ echo="Please wait!"
 bash $L/sys.sh
 echo "861607619"
 fi
+
+mkdir /usr/evekangle/
+touch /usr/evekangle/lock.img
